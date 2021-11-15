2f20f9ff02f2a4911f383aa6860707bb8e1cb1cc
Rick Martin <rmartin1985@att.net>
2021-11-15 17:55:00 -0600

    GET route for one user by Id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
+// GET all users
 router.get('/', (req, res) => {
     User.findAll({
         attributes: { exclude: ['password'] }
@@ -10,4 +11,52 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+// GET one user by Id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: [
+                    'id',
+                    'title',
+                    'content',
+                    'created_at'
+                ]
+            },
+            {
+                model: Comment,
+                attributes: [
+                    'id', 
+                    'comment_content',
+                    'created_at'
+                ],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: "No user found matching this id" });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
