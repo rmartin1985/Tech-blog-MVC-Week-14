370f78aee9b504109763c22ace4b03a23ee3e199
Rick Martin <rmartin1985@att.net>
2021-11-16 08:38:25 -0600

    GET route for a single post by id

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -14,9 +14,45 @@ router.get('/', (req, res) => {
         ],
         include: [
             {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_content',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
                 model: User, 
                 attributes: ['username']
-            },
+            }
+        ] 
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// GET one post by id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
             {
                 model: Comment,
                 attributes: [
@@ -30,13 +66,22 @@ router.get('/', (req, res) => {
                     model: User,
                     attributes: ['username']
                 }
+            },
+            {
+                model: User,
+                attributes: ['username']
             }
-        ] 
+        ]
     })
-    .then(dbPostData => res.json(dbPostData))
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post found matching that id." });
+            return;
+        }
+        res.json(dbPostData);
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
 });
-
