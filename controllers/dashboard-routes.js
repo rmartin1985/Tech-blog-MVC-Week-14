2b0255754dfc04631c2c6d086adc17af179b2a02
Rick Martin <rmartin1985@att.net>
2021-11-16 11:55:07 -0600

    route to render edits to the dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -45,3 +45,50 @@ router.get('/', withAuth, (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// Route to render edits to the dashboard
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_content',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No post found that matches that id." });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+            res.render('edit-post', { post, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
