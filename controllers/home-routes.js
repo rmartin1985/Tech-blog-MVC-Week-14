64f04cb7112fa0265fb54ac634fa42b5b50912d1
Rick Martin <rmartin1985@att.net>
2021-11-16 11:26:21 -0600

    GET route to render posts on the homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const sequelize = require('../config/connection');
+
+// GET all posts for the homepage
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_content',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
