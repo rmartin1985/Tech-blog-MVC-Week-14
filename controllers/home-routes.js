caba94d9462e7b2884bb4d9375bb18e6497080d1
Rick Martin <rmartin1985@att.net>
2021-11-16 11:40:49 -0600

    Route to render the comments on posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -107,3 +107,51 @@ router.get('/post/:id', (req, res) => {
         });
 });
 
+// Route to get comments of a post
+router.get('/posts-comments', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_content',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: "No post found that matchess this id." });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+            res.render('posts-comments', { posst, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
