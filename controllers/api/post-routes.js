c7728fa883df564971296cb837c1aeaf313dd7e7
Rick Martin <rmartin1985@att.net>
2021-11-16 08:48:15 -0600

    DELETE a post by id route created

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -122,3 +122,25 @@ router.put('/:id', (req, res) => {
             res.status(500).json(err);
         });
 })
+
+// DELETE a post route
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post found that matches that id." });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
